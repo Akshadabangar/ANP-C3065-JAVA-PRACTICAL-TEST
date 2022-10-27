ea718245563367c9443888d21a52330bc205acc9
akshada bangar <akshada bangar@LAPTOP-FK0NLA2C>
2022-10-27 22:52:03 +0530

    version1

--- JavaProject/src/question1/ArrayDemo.java
+++ JavaProject/src/question1/ArrayDemo.java
@@ -2,30 +2,23 @@ package question1;
 
 public class ArrayDemo 
 {
-	void setIntegerArray(int[] array) {
-		for(int i=0;i<array.length;i++) {
-		System.out.println("Array Elements at index: "+i+ " are: "+array[i]);
+	public void  manipulateIntegerArray (int arr[])
+	{											
+	for(int  i=0;i<arr.length;i++) 
+		{
+			System.out.println(arr[i]+5);
 		}
-	}
-	
-	int[] manipulateIntegerArray(int[] array) {
-	
-		int[] newArray = new int[array.length];
-		for(int i=0; i<array.length; i++) {	
-			newArray[i] = array[i]+5;
-			System.out.println("Array Elements Manipulated at index: "+i+ " are: "+newArray[i]);
-		}
-		return newArray;
-	}
+	}	
 	
 public static void main(String[] args) {
-		
-		ArrayDemo arrobj=new ArrayDemo();
-		int arr[]= {10,20,30,40,50};
-	  
-	    arrobj.setIntegerArray(arr);
-        arrobj.manipulateIntegerArray(arr);
-
+	
+	//method call
+	int arr[]=new int [1];
+	arr[0]=1;
+	
+	
+	ArrayDemo obj = new ArrayDemo();  
+	obj.manipulateIntegerArray(arr);
 }
 }
 
