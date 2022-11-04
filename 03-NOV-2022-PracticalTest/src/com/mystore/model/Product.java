1fe0d744c7210c958a83880d2605907e9f2cd06a
akshada bangar <akshada bangar@LAPTOP-FK0NLA2C>
2022-11-04 21:49:06 +0530

    Version1

---
+++ 03-NOV-2022-PracticalTest/src/com/mystore/model/Product.java
@@ -0,0 +1,63 @@
+package com.mystore.model;
+
+public class Product {
+	// Member Variable
+	int productId;
+	String productName;
+	double productPrice;
+
+	public Product() // Constructor Without Parameter
+	{
+
+	}
+
+	public Product(int pId, String pName, double pprice) //Constructor  With Parameter
+	{
+		productId = pId;
+		productName = pName;
+		productPrice = pprice;
+	}
+
+	public Product(int pId) {
+		productId = pId;
+	}
+
+	public Product(String pName) {
+		productName = pName;
+	}
+
+	public Product(double pprice) {
+		productPrice = pprice;
+	}
+
+	public void setProductId(int pId) { // set method
+		productId = pId;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void Product(String pName) {
+		productName = pName;
+	}
+
+	public void setProductName(String pName) {
+		productName = pName;
+
+	}
+
+	public String getProductName() // get method
+	{
+		return productName;
+	}
+
+	public void setProductPrice(double pprice) {
+		productPrice = pprice;
+	}
+
+	public double getProductPrice() {
+		return productPrice;
+	}
+
+}
